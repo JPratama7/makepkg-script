b3934895c5e341f2e350563ac74c6b4d9d6186e8
Jose Chasey Pratama <85815288+JPratama7@users.noreply.github.com>
2024-07-25 11:10:47 +0700

    add script to deploy

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -v /etc/makepkg.conf
+rm -v /etc/pacman.conf
+
+FOLDER="$1"
+
+if [ "$FOLDER" != "clanged" ] && [ "$FOLDER" != "gcced" ]; then
+    echo "Wrong folder"
+    exit 1
+fi
+
+cd $FOLDER
+
+# Copy new configuration files
+cp makepkg.conf /etc/
+cp pacman.conf /etc/
+
+# Update packages and initialize keys
+pacman -Syyu --noconfirm archlinux-keyring reflector
+pacman-key --init
+pacman-key --populate
+
+# Update mirror list
+reflector --ipv4 --ipv6 -l 10 -f 10 -a 10 --protocol http,https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Install additional packages
+pacman -Syyuu --noconfirm --needed git base-devel aria2-git
+
+# Copy entrypoint script
+cp entrypoint.sh /entrypoint.sh
+
+# Set entrypoint script as executable
+chmod +x /entrypoint.sh
+
+# Execute entrypoint script
+exec /entrypoint.sh
